c3f1dc324b8e15409917d312958fb8270f50d5dd
Kaua Hoepers <hopersdev@gmail.com>
2025-10-29 01:41:03 -0300

    feat(parte 4): incluindo um log para gerenciar erros, cadastros e edições

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+import fs from 'fs';
 import sqlite3 from 'sqlite3';
 import { open } from 'sqlite';
 
@@ -19,9 +20,19 @@ async function conectar() {
 
 async function consumirAPI() {
 
-  try {
+  // Iniciar log
+  const date_now = new Date();
+  const log = {
+    total: 0,
+    add: 0,
+    update: 0,
+    ignore: 0,
+    error: []
+  };
 
-    // Realizar a chamada na API
+  try { 
+
+    // Realizar a chamada na API   
     const cont = {};
     const response = await fetch('https://randomuser.me/api/?results=150');
     
@@ -31,9 +42,10 @@ async function consumirAPI() {
 
     // Realizar a montagem das informações trazidas na const data
     const data = await response.json();
-    const users = data.results;   
+    const users = data.results;  
 
-    const date_now = new Date();
+    log.total = users.length;
+
     const data_process = users.map((user) => {
 
       // Tratamento para buscar a idade de acordo com a data de nascimento
@@ -50,6 +62,9 @@ async function consumirAPI() {
     // Filtro para retornar apenas users com 18 ou mais
     }).filter((u) => u.age >= 18);  
        
+    // Incluir em logs os registros ignorados
+    log.ignore = users.length - data_process.length;
+
     data_process.forEach((u) => { 
        
       cont[u.country] = (cont[u.country] || 0) + 1;
@@ -58,7 +73,7 @@ async function consumirAPI() {
 
     const db = await conectar();
 
-    // criar a 
+    // Criar a tabela caso ela não exista
     await db.exec(`
       CREATE TABLE IF NOT EXISTS users_random (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -69,16 +84,31 @@ async function consumirAPI() {
       )
     `);
 
-    // Incluir informações dos usuários no banco de dados
+    // Incluir ou alterar informações dos usuários no banco de dados
     for (const d of data_process) {
-      await db.run(`
-        INSERT INTO users_random (name, email, country, age)
-        VALUES (?, ?, ?, ?)
-        ON CONFLICT(email) DO UPDATE SET
-          name = excluded.name,
-          country = excluded.country,
-          age = excluded.age
-      `, [d.name, d.email, d.country, d.age]);
+
+      try {
+
+        const email = d.email.trim().toLowerCase();
+        const select = await db.get(`SELECT * FROM users_random WHERE email = ?`, [email]);
+
+        if (select) {
+          await db.run(`UPDATE users_random SET name=?, country=?, age=? WHERE email=?`, 
+            [d.name, d.country, d.age, d.email]);
+          log.update += 1;
+
+        } else {
+          await db.run(`INSERT INTO users_random (name,email,country,age) VALUES (?,?,?,?)`,
+            [d.name, d.email, d.country, d.age]);
+          log.add += 1;
+
+        }
+
+      } catch (err) {
+        log.error.push({ email: d.email, mensagem: err.message });
+
+      }
+
     }
 
     // Mostrar soma de paises apresentados na relação de usuários
@@ -90,7 +120,25 @@ async function consumirAPI() {
 
     await db.close();
 
+    // Nome do arquivo de relatório com data/hora
+    const timestamp = date_now.toISOString().split('T')[0];
+    const log_path = `logs/${timestamp}.txt`;
+
+    // Gerar conteudo do arquivo de log e gravar
+    const text_log = `
+      LOG ${new Date().toLocaleString()}
+      
+      Adicionados: ${log.add} - Atualizados: ${log.update} - Ignorados: ${log.ignore}
+      Com Erros: ${log.error.map(e => `- ${e.email}: ${e.mensagem}`).join('\n')}
+      Total de registros gravados: ${log.total}
+      Total de registros no banco atualmente: ${total.total}
+      `;
+
+    fs.appendFileSync(log_path, text_log.trim()  + '\n');
+
   } catch (error) {
+    // Incluir em logs os registros com erro
+    log.error.push(`mensagem: ${error.message}`);
     console.error('Erro:', error.message);
   }
 
