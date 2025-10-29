5a84570747a6c873e47a6ee6ad8bf6cea60e0958
Kaua Hoepers <hopersdev@gmail.com>
2025-10-29 00:27:57 -0300

    feat(parte 2): incluir conexão com banco sqlite, e utilização

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
+import sqlite3 from 'sqlite3';
+import { open } from 'sqlite';
+
+// Função para abrir conexão com banco de dados
+async function conectar() {
+
+  try {
+
+    return open({
+        filename: './database.db',
+        driver: sqlite3.Database
+    });
+ 
+  } catch (error) {
+    console.error('Erro:', error.message);
+  }
+
+}
+
 async function consumirAPI() {
 
   try {
@@ -14,18 +33,62 @@ async function consumirAPI() {
     const data = await response.json();
     const users = data.results;   
 
-    const data_process = users.map((user) => ({
-      name: `${user.name.first} ${user.name.last}`,
-      email: user.email,
-      country: user.location.country,
-    }));  
-    
-    data_process.forEach((u) => {  
+    const date_now = new Date();
+    const data_process = users.map((user) => {
+
+      // Tratamento para buscar a idade de acordo com a data de nascimento
+      const date_age = new Date(user.dob.date);
+      const age = date_now.getFullYear() - date_age.getFullYear();
+
+      return {
+        name: `${user.name.first} ${user.name.last}`,
+        email: user.email,
+        country: user.location.country,
+        age
+      };
+
+    // Filtro para retornar apenas users com 18 ou mais
+    }).filter((u) => u.age >= 18);  
+       
+    data_process.forEach((u) => { 
+       
       cont[u.country] = (cont[u.country] || 0) + 1;
+
     });
+
+    const db = await conectar();
+
+    // criar a 
+    await db.exec(`
+      CREATE TABLE IF NOT EXISTS users_random (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL,
+        email TEXT UNIQUE NOT NULL,
+        country TEXT,
+        age INTEGER
+      )
+    `);
+
+    // Incluir informações dos usuários no banco de dados
+    for (const d of data_process) {
+      await db.run(`
+        INSERT INTO users_random (name, email, country, age)
+        VALUES (?, ?, ?, ?)
+        ON CONFLICT(email) DO UPDATE SET
+          name = excluded.name,
+          country = excluded.country,
+          age = excluded.age
+      `, [d.name, d.email, d.country, d.age]);
+    }
 
     // Mostrar soma de paises apresentados na relação de usuários
     console.log('Contagem de Países: ', cont);
+
+    // Buscar o total de arquivos registrados no banco de dados
+    const total = await db.get('SELECT COUNT(*) AS total FROM users_random');
+    console.log('Total de registros gravados: ', total.total);
+
+    await db.close();
 
   } catch (error) {
     console.error('Erro:', error.message);
